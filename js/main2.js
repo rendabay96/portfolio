b8a16bd58786c2f612b2b45f2cf66d4dd1606dd3
Brenda Hu <hbrendax4@gmail.com>
2020-04-29 16:38:17 -0400

    updated insights case study

---
+++ js/main2.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function() {
+    //smooth scroll
+	$('a').smoothScroll({
+		speed: 520
+	});
+    
+    //navbar transition
+    var header = $(".navbarGray");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 800) {
+            header.removeClass('navbarGray').addClass("navbarLight");
+        } else {
+            header.removeClass("navbarLight").addClass('navbarGray');
+        }
+    });
+    
+
+    
+    
+});
+
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-91000029-1', 'auto');
+  ga('send', 'pageview');
