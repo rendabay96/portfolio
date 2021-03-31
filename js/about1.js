3c0c25ff5f3371d2eb919ce9b9c49c25b10582f7
Brenda Hu <hbrendax4@gmail.com>
2021-03-31 01:09:30 -0400

    modified home behavior

--- js/about1.js
+++ js/about1.js
@@ -1,6 +1,5 @@
 function onSubmit() {
-
-         if (document.getElementById('my-designs').value == 'lessismore') {window.button2.style.display="block"; }else{ alert('Access Denied, Please try again');}
-     }
+    alert('Access Denied, Please try again');
+}
      
 
